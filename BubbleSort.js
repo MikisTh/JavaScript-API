cac609a426138acb6f1834b7f4b036b87c584ee8
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-22 11:21:24 -0300

    Create BubbleSort.js
    Add

---
+++ BubbleSort.js
@@ -0,0 +1,20 @@
+function bubbleSort(arr) {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                // Swap elements
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return arr;
+}
+
+// Example usage
+let array = [5, 3, 8, 4, 2];
+console.log(bubbleSort(array)); // Output: [2, 3, 4, 5, 8]
