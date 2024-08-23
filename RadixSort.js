174ed781444eebba2769bca235d03acace400c9f
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-23 11:39:25 -0300

    Create RadixSort.js
    Add

---
+++ RadixSort.js
@@ -0,0 +1,12 @@
+function radixSort(nums) {
+  let maxDigitCount = mostDigits(nums);
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < nums.length; i++) {
+      let digit = getDigit(nums[i], k);
+      digitBuckets[digit].push(nums[i]);
+    }
+    nums = [].concat(...digitBuckets);
+  }
+  return nums;
+}
