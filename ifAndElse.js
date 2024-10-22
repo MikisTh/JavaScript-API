6857da6de81c33fc3dbf1f5ef6ced8bfdb841d05
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-10-22 08:50:18 -0300

    Update ifAndElse.js

--- ifAndElse.js
+++ ifAndElse.js
@@ -1,18 +1,18 @@
-if ( Arroz, pão, massa, batata, mandioca >= 6 porcoes – 900 kcal ) {
+if ( ArrozPãoMassaBatataMandioca >= 6 porcoes – 900 kcal ) {
     console.log("Limite de consumo atingido!")
-} else if ( Legumes e verduras >= 3 porcoes – 45 kcal ) {
+} else if ( LegumesVerduras >= 3 porcoes – 45 kcal ) {
     console.log("Limite de consumo atingido!")
 } else if (Frutas >= 3 porcoes – 210 kcal){
     console.log("Limite de consumo atingido!")    
-} else if (Carnes e ovos >= 1 porção – 190 kcal) {
+} else if (CarnesOvos >= 1 porcao – 190 kcal) {
     console.log("Limite de consumo atingido!")
-} else if ( Leite, queijo e iogurte >= 3 porções – 360 kcal) {
+} else if ( LeiteQueijoIogurte >= 3 porcoes – 360 kcal) {
     console.log("Limite de consumo atingido!")
-} else if ( Feijões >= 1 porção – 55 kcal) {
+} else if ( leguminosas >= 1 porcao – 55 kcal) {
     console.log("Limite de consumo atingido!")
-} else if ( Óleos e gordura >= 1 porção – 73kcal) {
+} else if ( OleosGordura >= 1 porcao – 73kcal) {
     console.log("Limite de consumo atingido!")
-} else if ( Açúcares e doces >= 1 porção – 110 kcal) {
+} else if ( AcucaresDoces >= 1 porcao – 110 kcal) {
     console.log("Limite de consumo atingido!")
 
 
