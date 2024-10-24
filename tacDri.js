d637dbbe5ce64773251c8f26c5a717825783f683
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-10-24 11:33:17 -0300

    Add files via upload

---
+++ tacDri.js
@@ -0,0 +1,36 @@
+let user;
+
+console.log (user);
+
+let food;
+ 
+ console.log(food);
+
+let registerFood;
+
+ console.log(registerFood);
+
+let diety;
+
+ console.log(diety);
+
+let taco;
+
+console.log(taco);
+
+let dri;
+
+console.log(dri);
+
+//calculo IMC
+
+var imc, peso, altura;
+
+function imc () {
+
+    imc = altura * peso^2;
+
+    console.log(imc);
+
+return (imc);
+}
